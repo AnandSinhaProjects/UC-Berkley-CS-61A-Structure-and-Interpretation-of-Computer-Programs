8c40b886f5b2b5177ee40c1492cbc768a8c3a836
“AnandSProjects” <anands.projects22@gmail.com>
2022-10-01 10:01:34 +0530

    New Elements Added

---
+++ hw02/fizzbuzz.py
@@ -0,0 +1,31 @@
+def fizzbuzz(n):
+    """
+    >>> result = fizzbuzz(16)
+    1
+    2
+    fizz
+    4
+    buzz
+    fizz
+    7
+    8
+    fizz
+    buzz
+    11
+    fizz
+    13
+    14
+    fizzbuzz
+    16
+    >>> result is None  # No return value
+    True
+    """
+    for i in range(1,n+1):
+        if i%3 == 0 and i%5==0:
+            print('fizzbuzz')
+        elif i%5 == 0:
+            print('buzz')
+        elif i%3 == 0:
+            print('fizz')
+        else:
+            print(i)
