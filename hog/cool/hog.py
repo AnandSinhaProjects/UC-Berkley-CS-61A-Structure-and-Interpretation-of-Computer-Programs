c53f9e4155f5710b4144a156da4f1a02ca8aa282
“AnandSProjects” <anands.projects22@gmail.com>
2022-10-03 20:24:25 +0530

    New Functions Added and Changed the placement for better input experience

--- hog/cool/hog.py
+++ hog/cool/hog.py
@@ -8,14 +8,15 @@ from sys import flags
 
 six_sided = 6
 four_sided = 4
-score_main = 0 # Player 1 Score
-score_opp = 0 # Player 2 Score
-turn_number = 0 # Total number of turns taken in the game.
+# score_main = 0 # Player 1 Score
+# score_opp = 0 # Player 2 Score
+# turn_number = 0 # Total number of turns taken in the game.
 pig_tail = lambda x: 2 * abs(int(str(x)[0]) - int(str(x)[1])) + 1 #Function followed for pig_tail -> 2 * abs(tens - ones) + 1 points
 # faces = int(input('Enter the number of faces -> '))
 # num_rolls = int(input('Number of rolls to play -> '))
-# total_score = int(input('Total score to play -> '))
-# outcomes = input('Either enter the list of outcomes or "random" -> ')
+#total_score = int(input('Total score to play -> '))
+#outcomes = input('Either enter the list of outcomes or "random" -> ')
+always_roll_5 = 5
 
 #----------------------------------------------------------------   
 
@@ -36,7 +37,7 @@ def dice_roll(num_rolls:int=3,faces:int=six_sided):
 
 # This function is used to start the game in both test and benchmark mode.
 #print('Follow format def start_game(num_rolls, faces, total_score, outcomes)')
-def start_game(num_rolls:int=0, total_score:int=100, faces:int=six_sided,  outcomes:str=''):
+def start_game(num_rolls:int=0, faces:int=six_sided,  outcomes:str=''):
     # faces = int(input('Enter the number of faces -> '))
     # num_rolls = int(input('Number of rolls to play -> '))
     # outcomes = input('Either enter the list of outcomes or "random" -> ')
@@ -46,7 +47,7 @@ def start_game(num_rolls:int=0, total_score:int=100, faces:int=six_sided,  outco
         outcomes = list(map(int, outcomes.split()))
     return outcomes
 
-#outcomes = start_game(num_rolls, total_score, faces, outcomes)
+# outcomes = start_game(num_rolls, total_score, faces, outcomes)
 
 # This function checks if the value i is 1 or not.
 
@@ -67,7 +68,7 @@ def tail_points(score_opp):
         return(pig_tail(score_opp))    
 
 #Calculates the total points perfectly
-def take_turn(outcomes):
+def take_turn(outcomes,score_opp):
     points_round = 0
     if len(outcomes) == 0:
         return tail_points(score_opp)
@@ -103,4 +104,49 @@ def square_update(score):
     if flag == True:
         return next_perfect_square(ips)
 
+def play(p1rolls, p2rolls, game_type:str,total_score=100):
+    faces = int(input('Enter the number of faces -> '))
+    #num_rolls = int(input('Number of rolls to play -> '))
+    turn_number = 0
+    score_main = 0
+    score_opp = 0
+    while score_main<total_score and score_opp<total_score:
+        print(turn_number)
+        outcomes = 'random'#input('Either enter the list of outcomes or "random" -> ')
+        round_score = 0
+        if game_type == 'square_update':
+            if turn_number % 2 == 0:
+                num_rolls = p1rolls
+                outcomes = start_game(num_rolls, faces, outcomes)
+                round_score = take_turn(outcomes, score_opp)
+                score_main += round_score
+                score_main = square_update(score_main)
+                turn_number += 1
+                print('Score Player 1 -> ',score_main)
+            else:
+                num_rolls = p2rolls
+                outcomes = start_game(num_rolls, faces, outcomes)
+                round_score = take_turn(outcomes, score_main)
+                score_opp += round_score
+                score_opp = square_update(score_main)
+                turn_number += 1
+                print('Score Player 2 -> ',score_main)
+        else:
+            if turn_number % 2 == 0:
+                num_rolls = p1rolls
+                outcomes = start_game(num_rolls, faces, outcomes)
+                round_score = take_turn(outcomes, score_opp)
+                score_main += round_score
+                turn_number += 1
+                print('Score Player 1 -> ',score_main)
+            else:
+                num_rolls = p2rolls
+                outcomes = start_game(num_rolls, faces, outcomes)
+                round_score = take_turn(outcomes, score_main)
+                score_main += round_score
+                turn_number += 1
+                print('Score Player 2 -> ',score_main)
 
+    return score_main, score_opp
+            
+            
