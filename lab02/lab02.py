351c81966c47d4b9997ebca2f0875c078486cbd0
“AnandSProjects” <anands.projects22@gmail.com>
2022-10-07 21:09:04 +0530

    New Functions Added and Changed the placement for better input experience

--- lab02/lab02.py
+++ lab02/lab02.py
@@ -13,9 +13,7 @@ def lambda_curry2(func):
     >>> lambda_curry2(mod)(123)(10)
     3
     """
-    "*** YOUR CODE HERE ***"
-    return ______
-
+    return func
 
 def lambda_curry2_syntax_check():
     """Checks that definition of lambda_curry2 is one line.
@@ -55,7 +53,7 @@ def count_cond(condition):
     >>> count_primes(20)   # 2, 3, 5, 7, 11, 13, 17, 19
     8
     """
-    "*** YOUR CODE HERE ***"
+    return condition
 
 
 def composer(f, g):
